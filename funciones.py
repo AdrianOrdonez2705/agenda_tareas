0481aeeadb27e16f4ed745b1a0ed9be6f29967c0
AdrianGonzo <adrian.gonzales.f@ucb.edu.bo>
2025-09-11 16:38:35 +0000

    Agregué la función agregar_tarea

--- funciones.py
+++ funciones.py
@@ -1,1 +1,9 @@
 # Este es el archivo de funciones
+tareas = []
+def agregar_tarea ():
+    nom_tarea = input("Nombre tarea")
+    est_tarea = "pendiente"
+    tarea = {Nombre: nom_tarea, Estado: est_tarea}
+    tarea.append(tarea)
+    print ("Tarea agregada")
+
