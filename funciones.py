2830dfaedd9ef72a4143979fb32fda200a4113d4
Adriana-Alvarez <adriana.alvarez.c@ucb.edu.bo>
2025-09-11 16:58:55 +0000

    Implemento función eliminar_tarea

--- funciones.py
+++ funciones.py
@@ -1,1 +1,14 @@
 # Este es el archivo de funciones
+
+# Funcion de eliminar la tarea
+def eliminar_tarea(numero):
+    if numero < 1 or < len(tareas):
+        print ("esta tarea no existe, revisa de nuevo su numero")
+        return
+    copiaTareas = tareas[numero - 1]
+    if copiaTareas ["estado"] == "Completada":
+        tareas.pop(numero - 1)
+        print ("Tarea eliminada con éxito")
+    else:
+        print ("Error al eliminar la tarea, solo se pueden borrar tareas en estado Completado")
+    
