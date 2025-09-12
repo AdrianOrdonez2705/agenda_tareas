4203dbf85eda66ff590e0677950dc3ddc767e3e1
Adriana-Alvarez <adriana.alvarez.c@ucb.edu.bo>
2025-09-12 00:57:11 -0400

    Corrijo funcion eliminar_tarea
    Se realizo un cambio en la ultima validaci´n de tarea marcada en completada para poder eliminarla

--- funciones.py
+++ funciones.py
@@ -1,17 +1,25 @@
 # Este es el archivo de funciones
 
-# Funcion de eliminar la tarea
-def eliminar_tarea(numero):
-    if numero < 1 or < len(tareas):
-        print ("esta tarea no existe, revisa de nuevo su numero")
-        return
+
+def agregar_tarea(): 
+    nombre = input("Ingrese el nombre de la tarea: ") 
+    tarea = { 
+        "Nombre" : nombre, 
+        "Estado": "Pendiente" 
+    } 
+    tareas.append(tarea) 
+    print("Tarea agregada correctamente\n") 
+
+def eliminar_tarea(numero): 
+    if numero < 1 or numero > len(tareas): 
+        print ("esta tarea no existe, revisa de nuevo su numero\n") 
+        return 
     copiaTareas = tareas[numero - 1]
-    if copiaTareas ["estado"] == "Completada":
+    if copiaTareas ["Estado"] == "Completada":
         tareas.pop(numero - 1)
         print ("Tarea eliminada con éxito")
     else:
         print ("Error al eliminar la tarea, solo se pueden borrar tareas en estado Completado")
-    
 
 tareas = []
 def agregar_tarea ():
